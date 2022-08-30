ed74436dc946630ff844ca2699ee998249bdf5f4
Agnideep <95609274+ag22bands@users.noreply.github.com>
2022-08-30 08:50:21 +0530

    Add files via upload

---
+++ og_temp.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ff first
+#define ss second
+#define ll long long
+#define zerobits(x) __builtin_ctzll(x)
+#define setbits(x) __builtin_popcountll(x)
+#define all(x) (x).begin(), (x).end()
+#define invectr(x)     \
+    for (auto &i : x) \
+        cin >> i;
+const int MAXN = 200200;
+
+ 
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        
+    }
+    return 0;      
+}
+
+//haze_12
---
+++ template1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ff first
+#define ss second
+#define ll long long
+#define zerobits(x) __builtin_ctzll(x)
+#define setbits(x) __builtin_popcountll(x)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define vi vector<int>
+#define invectr(x)     \
+    for (auto &i : x) \
+        cin >> i;
+const int MAXN = 200200;
+
+ 
+void solve(){
+	
+}
+ 
+int main(){
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+	int T;
+	cin >> T;
+	while(T--) solve();
+}
+//haze_12
