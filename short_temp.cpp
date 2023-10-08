7ef5560d40a2583dc61e29f4b9ed309dde26418c
Agnideep <95609274+ag-wnl@users.noreply.github.com>
2023-10-08 12:36:41 +0530

    Create short_temp.cpp

---
+++ short_temp.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = int64_t;    
+#define all(x) (x).begin(), (x).end()
+const int MAXN = 1e5+5;
+
+
+void solve(){
+    
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    int T;
+    cin >> T;
+    while(T--) solve();
+}
