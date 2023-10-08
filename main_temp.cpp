d867dea87b2855af2ae943709137598d478d5383
Agnideep <95609274+ag-wnl@users.noreply.github.com>
2023-10-08 12:37:08 +0530

    Create main_temp.cpp

---
+++ main_temp.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define trav(a,x) for (auto& a : x)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const int MOD = 1000000007;
+const int MAXN = 200200;
+
+void solve(){
+
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);    
+ 
+    int T = 1;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+ 
+    return 0;
+}
