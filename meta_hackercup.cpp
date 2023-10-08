7e1f1f26168c0595e9deb0354968ad40793ec151
Agnideep <95609274+ag-wnl@users.noreply.github.com>
2023-10-08 12:35:02 +0530

    Create meta_hackercup.cpp

---
+++ meta_hackercup.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = int64_t;    
+#define all(x) (x).begin(), (x).end()
+const int MAXN = 1e5+5;
+
+
+void solve(int T){
+    cout << "Case #" << T << ": ";
+    
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    int T;
+    cin >> T;
+    for(int z=1; z<=T; z++){
+        solve(z);
+    }
+}
