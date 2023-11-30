4849bfb5dedc0a8717e870317e53c1e68e175d8d
Agnideep Ghosh <95609274+ag-wnl@users.noreply.github.com>
2023-11-30 15:45:16 +0530

    Update fastpow.cpp

--- fastpow.cpp
+++ fastpow.cpp
@@ -3,10 +3,10 @@
 long long fastpow(long long n, long long x, long long m){
   long long ret = 1;
   while(x){
-  if(x&1)
-   (ret*=n)%=m;
-  x>>=1;
-  (n*=n)%=m;
+    if(x&1)
+     (ret*=n)%=m;
+    x>>=1;
+    (n*=n)%=m;
   }
   return ret;
 }
