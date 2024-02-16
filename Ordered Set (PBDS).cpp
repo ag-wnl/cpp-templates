61b370ae8ae84be31c20fa0e6011a2f8ad722800
Agnideep Ghosh <95609274+ag-wnl@users.noreply.github.com>
2024-02-16 09:43:46 +0530

    Create Ordered Set (PBDS).cpp

---
+++ Ordered Set (PBDS).cpp
@@ -0,0 +1,8 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
+// Usage:
+// ordered pair st; // to find index of a value : st.order_of_key(value);
+
